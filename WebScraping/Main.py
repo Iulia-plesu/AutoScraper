ec0509a6c74c7f80d372044281f6be908936e2f2
iuliap710 <111219535+iuliap710@users.noreply.github.com>
2025-04-10 22:25:44 +0300

    wait a max of 10 seconds until the page opens in the browser

--- WebScraping/Main.py
+++ WebScraping/Main.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import json
-import time
 
 url = "https://www.thetimes.com/"
 
@@ -12,15 +14,19 @@ def scrape_data():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get(url)
 
-    time.sleep(5)
+    try:
+        wait = WebDriverWait(driver, 10)
+        titles_element = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "article-title")))
 
-    titles = driver.find_elements(By.CLASS_NAME, 'article-title')
+        titles = [title.text for title in titles_element]
 
-    title_texts = [title.text for title in titles]
+    except Exception as e:
+        print(f"Error: {e}")
+        titles = []
 
     driver.quit()
 
-    return json.dumps(title_texts)
+    return json.dumps(titles)
 
 if __name__ == "__main__":
     print(scrape_data())
